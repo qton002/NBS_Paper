37f5a222679a232edcc1d8cd4a97da4f917aeaa0
qton002 <103477295+qton002@users.noreply.github.com>
2022-04-12 23:44:48 +1200

    Update 1_akl_sentLevel_analysis.py

--- 1_akl_sentLevel_analysis.py
+++ 1_akl_sentLevel_analysis.py
@@ -1,3 +1,4 @@
+from inspect import getfile
 import pandas as pd
 import nltk
 import re
@@ -10,13 +11,62 @@ from tqdm import tqdm
 import nltk.corpus as Corpus
 import numpy as np
 
+##_________________________________Delete Agency-Related Sentences (start)_____________________________##
+from collections import Counter
+with open("list_of_agency_related_info.txt") as input_file:
+    filestr = ''.join(w for w in input_file)
+    cleanstr = re.sub(r'[^\w\s]',' ',filestr)
+    wordTokens = word_tokenize(cleanstr)
+    #print(wordTokens)
+    stop_words = stopwords.words('english')
+    cleanedTokens=[w.lower() for w in wordTokens if not w.lower() in stopwords.words('english')]
+    count=Counter(word for line in cleanedTokens
+                        for word in line.split())
+common50s=count.most_common(50)    
+#print(common50s)
+input_file.close()
+
+agentrelated_words = ['contact', 'email', 'call', 'please', 'available', 'help', 'arrange', 'viewing', 'estate', 'specialists', 'pricing', 'negotiation']
+socialmedia = ['facebook','twitter','linkedin','instagram','com','nz']
+agencyname = ['cbre','frank','metro','colliers']
+
 file = pd.read_csv("1_akl_test2 - Copy.csv")
 ad_descriptions = file.Ad_Desc.values
-        
+
 seismic_related_sents=[]
 seismic_related_sents_array=[]
 
-##_________________________________seismic-related sentences (start)_____________________________##
+def check_pres(sub_sents, agencyWord):
+    for sents in sub_sents:
+        for words in agencyWord:
+            if words in sents:
+                return 0
+    return 1
+    
+i=0
+for sentences in ad_descriptions:
+    sentences = sent_tokenize(ad_descriptions[i])
+    sentence_arrays = [sentence.split('\n') for sentence in sentences]
+    split_sentences = []
+    for sub_list in sentence_arrays:
+        for sentence in sub_list:
+            sentence = sentence.strip()
+            split_sentences.append(sentence.lower())
+    split_sents= ' '.join([sents for sents in split_sentences])
+
+for sents in split_sentences:
+    if check_pres(split_sentences, agentrelated_words):
+        split_sentences.remove(sents.lower())
+        print("the sentence removed is:", sents)
+
+##_________________________________Delete Agency-Related Sentences (end)_____________________________##
+''' 
+###_________________________________seismic-related sentences (start)_____________________________##
+file = pd.read_csv("1_akl_test2 - Copy.csv")
+ad_descriptions = file.Ad_Desc.values
+
+seismic_related_sents=[]
+seismic_related_sents_array=[]
 i=0
 for sentences in ad_descriptions:
     sentences = sent_tokenize(ad_descriptions[i])
@@ -63,7 +113,7 @@ for sentences in ad_descriptions:
             seismic_related_sents.append(eachSent.strip())
             seismic_related_sents.append(earthquake_Pos)
             seismic_related_sents_array.append([i,eachSent.strip(), earthquake_Pos])
-        elif 'iep' in eachSent.lower(): ########################################################################################################### word 'iep'
+        if 'iep' in eachSent.lower(): ########################################################################################################### word 'iep'
 ##            print(i," ",eachSent, "iep yes")
             res_iep = res.index('iep')+1
 ##            print(i," ","The location of 'iep' in sentence is : " + str(res_iep))
@@ -80,7 +130,7 @@ for sentences in ad_descriptions:
             seismic_related_sents.append(eachSent.strip())
             seismic_related_sents.append(iep_Pos)   
             seismic_related_sents_array.append([i,eachSent.strip(), iep_Pos])
-        elif 'seismic' in eachSent.lower(): ########################################################################################################### word 'seismic'
+        if 'seismic' in eachSent.lower(): ########################################################################################################### word 'seismic'
 ##            print(i," ",eachSent, "seismic yes")
             res_seismic = res.index('seismic')+1
 ##            print(i," ","The location of 'seismic' in sentence is : " + str(res_seismic))
@@ -97,7 +147,7 @@ for sentences in ad_descriptions:
             seismic_related_sents.append(eachSent.strip())
             seismic_related_sents.append(seismic_Pos)   
             seismic_related_sents_array.append([i,eachSent.strip(), seismic_Pos])
-        elif 'nbs' in eachSent.lower(): ########################################################################################################### word 'nbs'
+        if 'nbs' in eachSent.lower(): ########################################################################################################### word 'nbs'
 ##            print(i," ",eachSent, "nbs yes")
             res_nbs = res.index('nb')+1
 ##            print(i," ","The location of 'nbs' in sentence is : " + str(res_nbs))
@@ -117,173 +167,11 @@ for sentences in ad_descriptions:
     i+=1
 ##print(seismic_related_sents_array)
 
-seismic_related_SentsandPosition = pd.DataFrame(seismic_related_sents_array, columns = ['Sents_Index','Seismic_Related_Sentences', 'Word_Position'])
-print(seismic_related_SentsandPosition)
-seismic_related_SentsandPosition.to_csv('1_alk_seismic_related_SentsandPosition.csv', encoding='utf-8', index=False)
+#seismic_related_SentsandPosition = pd.DataFrame(seismic_related_sents_array, columns = ['Sents_Index','Seismic_Related_Sentences', 'Word_Position'])
+#print(seismic_related_SentsandPosition)
+#seismic_related_SentsandPosition.to_csv('1_alk_seismic_related_SentsandPosition.csv', encoding='utf-8', index=False)
 
-##vectors_df = pd.DataFrame(
-##    (count, word) for word, count in
-##    zip(wd_freq.tolist()[0],
-##        feature_names_vectors))
-##vectors_df.columns = ['Words','TF_iDF']
-##vectors_df.sort_values('TF_iDF', ascending=False,inplace=True)
-##
-###print(vectors_df.head())
-##print("vectors_df:", vectors_df)
-##
-##vectors_df.to_csv('1_akl_TF_iDF_keywords.csv', encoding='utf-8', index=False)
-
-'''i=0
-numOfAds=len(ad_descriptions)
-print("number of ads in total", numOfAds)
-#print("testing testing ad_descriptions[0]", ad_descriptions[0])
-for i in range(2):
-#for i in range(numOfAds):
-    eachSent = tokenizer.tokenize(ad_descriptions[i])
-    #eachSent = sent_tokenize(ad_descriptions[i])
-    #print("eachSent", eachSent)
-    numOfsentsinAd=len(eachSent)
-    print("numOfsentsinAd", numOfsentsinAd)
-    sents= ' '.join([sents for sents in eachSent])
-    numOfwordsinAd = len(sents.split())
-    print("numOfwordsinAd", numOfwordsinAd)
-    current_Sent=eachSent[0].lower()
-    print("current_Sent ", current_Sent)
-    i+=1
-print("seismic_related_sents", seismic_related_sents)
-
-    for j in range(numOfsentsinAd):
-        current_Sent=eachSent[j].lower()
-        print("current_Sent[",i,j,"].lower(): ",current_Sent)
-        print(current_Sent.index("earthquake"))
-        try:
-            if eachSent.find("earthquake"):            
-                seismic_related_sents.append(i)
-                seismic_related_sents.append(j)
-                seismic_related_sents.append(eachSent[j].strip())
-            j+=1
-        except Exception:
-            print("earthquake not in the sentence")'''
-##    i+=1
-##print("seismic_related_sents", seismic_related_sents)
-#for s in seismic_related_sents:
-  #  print(s)
-##_________________________________seismic-related sentences (end)_____________________________##
-'''
-            res = re.sub("[^\w]", " ",  sents).split()
-            earthquakeYN=None
-            seismicYN=None
-            nbsYN=None
-            iepYN=None
-        try:
-            if eachSent[j].lower().index("earthquake") >1:
-                res_earthquake = res.index("earthquake") + 1
-                earthquakeYN=True
-                print("The location of 'earthquake' in sentence", i,j, " is : " + str(res_earthquake))
-        except Exception:
-            print("The location of 'earthquake' in sentence has", i,j, "earthquake error")
-        try:
-            if eachSent[j].lower().index('seismic') >1:
-                res_seismic = res.index('seismic') + 1
-                seismicYN=True
-                print("The location of 'seismic' in sentence", i,j, " is : " + str(res_seismic))
-        except Exception:
-            print("The location of 'seismic' in sentence has", i,j, "seismic error")
-        try:
-            if eachSent[j].lower().index('nbs') >1:
-                res_nbs = res.index('nbs') + 1
-                nbsYN = True
-                print("The location of 'nbs' in sentence", i,j, "  is : " + str(res_nbs))
-        except Exception:
-            print("The location of 'nbs' in sentence has", i,j, "nbs error")
-        try:
-            if eachSent[j].lower().index('iep') >1:
-                res_iep = res.index('iep') + 1
-                iepYN = True
-                print("The location of 'iep' in sentence", i,j, "  is : " + str(res_iep))
-        except Exception:
-            print("The location of 'iep' in sentence has", i,j, "iep error")'''
-
-
-'''          wordindex = eachSent[j].lower().find('earthquake' or 'seismic' or 'nbs' or 'iep')
-            #print("len(ad_descriptions[",i,j,"]",len(ad_descriptions[i]))
-            if wordindex/len(ad_descriptions[i]) <= 1/3:
-                seismic_related_sents.append("Pos_Begining")
-            elif wordindex/len(ad_descriptions[i]) > 1/3 and wordindex/len(ad_descriptions[i]) <= 2/3:
-                seismic_related_sents.append("Pos_Middle")
-            else:
-                seismic_related_sents.append("Pos_End")    '''
-
-'''
-        if earthquakeYN==True and seismicYN==True:
-            print("both earthquakeYN and seismicYN is True")
-        elif earthquakeYN==True and nbsYN==True:
-            print("both earthquakeYN and nbsYN is True")
-        elif earthquakeYN==True and iepYN==True:
-            print("both earthquakeYN and iepYN is True")
-        elif seismicYN==True and nbsYN==True:
-            print("both seismicYN and nbsYN is True")
-        elif seismicYN==True and iepYN==True:
-            print("both seismicYN and iepYN is True")        
-        elif seismicYN==True and iepYN==True:
-            print("both nbsYN and iepYN is True")
-        else:
-            print("seismic related words only appeared once in ad_desc[",i,j,"]")
-
-        try:
-            if res_earthquake<res_seismic and res_nbs and res_iep:
-                res = res_earthquake
-                print("The location of 'res_earthquake' in sentence", i,j, "  is : " + str(res_earthquake))
-            elif res_seismic<res_earthquake and res_nbs and res_iep:
-                print("The location of 'res_seismic' in sentence", i,j, "  is : " + str(res_seismic))                
-            elif res_nbs<res_earthquake and res_seismic and res_iep:
-                print("The location of 'res_nbs' in sentence", i,j, "  is : " + str(res_nbs))
-            elif res_iep<res_earthquake and res_seismic and res_nbs:
-                print("The location of 'res_iep' in sentence", i,j, "  is : " + str(res_iep))
-            else:
-                print("no seismic related words exists in the sentence",i,j)
-        except:
-            pass'''
-##        else:
-##            print ("This sentence does not contain a seismic-related word.")
-##            res = re.sub("[^\w]", " ",  sents).split()
-##            res = res.index(wrd) + 1
-##            # printing result 
-##            print("The location of word is : " + str(res)) 
-
-
-
-#numOfsentsinAd=len(ad_descriptions[i])-1
-#sents= ' '.join([sents for sents in ad_descriptions[i]])       
-#numOfwordsinAd = len(sents.split())
-
-eachAd_index= 0
-'''for eachAd_index in range(len(ad_descriptions)-1):
-    len_eachAd = len(ad_descriptions[eachAd_index].split())-1 #word count for each ad
-    i=0
-    for i in range(len_eachAd):   
-        eachSent = ad_descriptions[eachAd_index][i]
-        numOfwords = len(eachSent.split())-1  #word count for each sentence in the ad
-        print("numOfwords for[ad_descriptions[",eachAd_index,"]", numOfwords)
-
-        if "earthquake" in eachSent.lower() or "seismic" in eachSent.lower() or "nbs" in eachSent.lower() or "iep" in eachSent.lower():
-            new_testing0.append(eachSent.strip())
-            print("testing0[",i,"].strip()",eachSent.strip())
-            i+=1
-        else:
-            print("no seismic related sentences.")
-            break
-    eachAd_index +=1'''
-        
-##for i in testing:
-##    print("testing", i)
-
-##tokenizer = nltk.tokenize.punkt.PunktSentenceTokenizer()
-##for eachSentence in ad_descriptions:
-##    #eachSentence = re.sub(r'[\*\;]', '.', eachSentence)
-##    #eachSentence = re.sub(r'[\-\–\·\•\…\_\'\"\’\”\‘\“]', ' ', eachSentence) #removed '+' to show 'A+ Gradings'
-##    sent_text = tokenizer.tokenize(eachSentence)
-##    print("sent_text", sent_text)
+##_________________________________seismic-related sentences (end)_____________________________### 
 
 ##_________________________________getting unique sentences (start)_____________________________##
 filename = "1_akl_test2.csv"  
@@ -319,20 +207,7 @@ for sents in sentences:
 
 ##for i in clean_sentences:
 ##    print(i)
-
-'''#seismic_sents = pd.read_csv("1_akl_sent_text_nodupli_seismic_only.csv")
-seismic_sents = pd.read_csv("1_akl_sent_text_nodupli.csv")   
-data = seismic_sents.seismic_sents.values
-clean_sents=[]
-#stop_words_remove = ['a', 'an','b','c']
-#stop_words = set(stopwords.words('english')).difference(stop_words_remove)
-for sents in data:
-    #filtered_sents = [w for w in word_tokenize(sents) if not w.lower() in stop_words]
-    filtered_sents = [w for w in word_tokenize(sents)]
-    filtered_sents = ' '.join([sents for sents in filtered_sents])
-    clean_sents.append(filtered_sents.strip())
-#print("clean_sents:", clean_sents)'''
-
+# 
 seismic_yes_only = []
 for i in clean_sentences:
     if "earthquake" in i or "seismic" in i or "nbs" in i or "iep" in i:
@@ -351,71 +226,8 @@ seismic_yes_noDups=list(dict.fromkeys(seismic_yes_only))
 seismic_yes_noDups.sort()
 
 #for i in seismic_yes_noDups:
-    #print(i)
-    
-
-'''icontainsi = []
-i=0
-icontainsi.append(seismic_yes_noDups[i])
-for i in range(len(seismic_yes_noDups)-1):
-    icontainsi.append(seismic_yes_noDups[i+1])
-    if re.search(seismic_yes_noDups[i],seismic_yes_noDups[i+1]):
-        #print("found")
-        icontainsi.remove(seismic_yes_noDups[i])
-    elif re.search(seismic_yes_noDups[i+1],seismic_yes_noDups[i]):
-        #print("found elif")
-        icontainsi.remove(seismic_yes_noDups[i+1])
-    i+=1
-icontainsi.sort()
-##for i in icontainsi:
-##    print(i)'''
-
-'''
-phrase_freq = []
-counter = 0
-i=0
-for i in range (len(icontainsi)-1):
-    j = 0
-    for j in range(len(clean_sentences)-1):
-        if icontainsi[i] in clean_sentences[j]:
-            counter +=1
-            #phrase_freq.append()
-            j += 1
-        else:
-            counter = counter
-            j+=1
-    i+=1
-    #print(icontainsi[i], ": ", counter)
-
-##_________________________________getting unique sentences (end)_____________________________##
-
-lemmatizer = WordNetLemmatizer()
-icontainsi_lemmatized = ' '.join([lemmatizer.lemmatize(w) for w in icontainsi])
-
-from sklearn.feature_extraction.text import CountVectorizer
-pd.set_option("display.max_rows", None, "display.max_columns", None) ##set this to display all results
-count_vec = CountVectorizer(analyzer='word', ngram_range=(3,5))
-count_occurs = count_vec.fit_transform([icontainsi_lemmatized])
-feature_names = count_vec.get_feature_names() ##in array
-feature_names_txt ='\n'.join([str(item) for item in feature_names])  ##in str
-#print(feature_names_txt2)
-
-count_occur_df = pd.DataFrame(
-    (count, word) for word, count in
-    zip(count_occurs.toarray().tolist()[0],
-    count_vec.get_feature_names_out()))
-count_occur_df.columns = ['Word2','Word_Freq']
-count_occur_df.sort_values('Word_Freq', ascending=False,inplace=True)
-
-print("count_occur_df: ",count_occur_df)
-#print(count_occur_df2.head())
-
-'''
-
-'''
-filename = "1_akl_test2.csv"  
-f = open(filename,"r",encoding="utf-8")
-ad_descriptions = f.read()
+    #print(i) 
+# ##_________________________________getting unique sentences (end)_____________________________##
 
 ##_________________________________sentences level analysis (OPEN)_____________________________##
 ad_descriptions = re.sub(r'[\*\;]', '\n', ad_descriptions)
@@ -434,8 +246,8 @@ for sub_list in sentences_arrays:
 ##df.sort_values('Ad_Desc', ascending=False,inplace=True)
 ##df .to_csv('1_akl_sent_text.csv', encoding='utf-8', index = False, header=False) #save to new file. Manully classify the words.
 ##________________________________
-'''
-'''vocab=nltk.FreqDist(sentences)
+
+vocab=nltk.FreqDist(sentences)
 print("Length of vocab: ", len(vocab))
 freq_tuple=vocab.most_common(200)
 freq_array = np.asarray(freq_tuple)
@@ -462,8 +274,8 @@ df_most_common.to_csv('1_akl_most_common200.csv', encoding='utf-8')
 ####_____________________________________________________________________________________####
 
 f.close()
-'''
-'''
+
+
 ###____________________________________________________####
 filename_sent_text = "1_akl_sent_text.csv"  
 with open(filename_sent_text,"r",encoding="utf-8") as file:
